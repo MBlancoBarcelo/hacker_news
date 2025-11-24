9411466f04675848a38570b94f23cdfcdc0a9244
MBlancoBarcelo <mblancobarcelo@gmail.com>
2025-11-24 16:55:30 +0100

    hacer .thne en ponerHistorias

--- src/main.js
+++ src/main.js
@@ -8,3 +8,19 @@ botones.forEach((element) => {
     element.classList.add("activado")
   })
 })
+
+ponerHistorias()
+
+
+function pillarHistorias() {
+  return fetch ("https://hacker-news.firebaseio.com/v0/topstories.json").then(response => response.json()).then((data) => {
+    console.log(data)
+    return data;
+  })
+}
+
+
+function ponerHistorias(){
+const data = pillarHistorias()
+  console.log(data)
+}
