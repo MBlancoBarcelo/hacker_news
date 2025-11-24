c296f30d76e5010c5ecba7458bac99c60c76441e
MBlancoBarcelo <mblancobarcelo@gmail.com>
2025-11-24 16:10:58 +0100

    commit inicial 2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+const botones = document.querySelectorAll(".botones")
 
+botones.forEach((element) => {
+  element.addEventListener("click", () => {
+    botones.forEach((element) => {
+      element.classList.remove("activado")
+    })
+    element.classList.add("activado")
+  })
+})
