41366acd92c44b7f6582f56125c77d45aa15a4a9
Marc <marc.blanco.barcelo@gmail.com>
2025-12-09 09:26:14 +0100

    FINAL I THINK

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 const botones = document.querySelectorAll(".botones")
 const stories = document.querySelector("#stories");
-const cosarara = document.querySelector("#prueba")
 const anterior = document.querySelector("#anterior")
 const siguiente = document.querySelector("#siguiente")
 const numeropagina = document.querySelector("#pagina")
-let idsnoticias
-let contador = 0;
-let contador2 = 3;
-let pagina = 1;
-let selectType = loadLocalSelect();
+const estadodelosbotones = document.querySelector("#anteriorsiguiente")
+let storyIds
+let currentPage = 0;
+let commentsPerPage = 3;
+let pageNumber = 1;
+let storyType = loadLocalSelect();
+let isNotReloading = true;
 
-numeropagina.textContent = pagina;
+
+numeropagina.textContent = pageNumber;
 
 function showLoader() {
+  estadodelosbotones.style.display = "none"
   stories.textContent="";
   let loaderDiv = document.createElement("div")
   loaderDiv.classList.add("loader-container")
@@ -23,6 +26,7 @@ function showLoader() {
 }
 
 function hideLoader() {
+  estadodelosbotones.style.display = ""
   let loader = document.querySelector(".loader-container")
   if(loader){
     loader.remove()
@@ -48,9 +52,9 @@ botones.forEach((element) => {
       element.classList.remove("activado")
     })
     element.classList.add("activado")
-    selectType = pickType(element.textContent);
-    localStorage.setItem("selecttype", selectType)
-    history.pushState({},"",selectType)
+    storyType = pickType(element.textContent);
+    localStorage.setItem("selecttype", storyType)
+    history.pushState({},"",storyType)
     ponerHistorias()
   })
 })
@@ -70,39 +74,40 @@ pressButon()
 ponerHistorias()
 
 siguiente.addEventListener(("click"), () => {
-  pagina++;
-  numeropagina.textContent = pagina;
-  contador += 10
+  pageNumber++;
+  numeropagina.textContent = pageNumber;
+  currentPage += 10
   ponerHistorias()
 })
 
 anterior.addEventListener(("click"), () => {
-  if (pagina == 1){
+  if (pageNumber == 1){
     return
   }
-  contador = contador - 10;
-  pagina--;
-  numeropagina.textContent = pagina
+  currentPage = currentPage - 10;
+  pageNumber--;
+  numeropagina.textContent = pageNumber
   ponerHistorias()
 })
 
 
 async function pillarHistorias() {
-  const response = await fetch(`https://hacker-news.firebaseio.com/v0/${selectType}.json`)
+  const response = await fetch(`https://hacker-news.firebaseio.com/v0/${storyType}.json`)
   return await response.json()
 }
 
 
 
 async function ponerHistorias() {
+  isNotReloading = false;
   showLoader()
 
-  idsnoticias = await pillarHistorias()
+  storyIds = await pillarHistorias()
 
-  const numerodenoticias = idsnoticias.slice(contador, contador + 10)
+  const storiesSlice = storyIds.slice(currentPage, currentPage + 10)
 
   const noticias = await Promise.all(
-    numerodenoticias.map(id =>
+    storiesSlice.map(id =>
       fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(response => response.json())
     )
   )
@@ -142,9 +147,9 @@ async function ponerHistorias() {
 
 
 
-    const skibidi = document.createElement("a")
-    skibidi.textContent = "Numero Comentarios: " + (noticia.kids?.length || 0)
-    skibidi.addEventListener("click", () => {
+    const commentsLink = document.createElement("a")
+    commentsLink.textContent = "Numero Comentarios: " + (noticia.kids?.length || 0)
+    commentsLink.addEventListener("click", () => {
       ponerComentarios(noticia.title, noticia.kids)
     })
 
@@ -154,13 +159,15 @@ async function ponerHistorias() {
 
 
     hideLoader()
+        estadodelosbotones.style.display = ""
+
     const divdecontenido = document.createElement("div")
     divdecontenido.classList.add("contenido")
 
     
     divdecontenido.appendChild(score)
     divdecontenido.appendChild(autor)
-    divdecontenido.appendChild(skibidi)
+    divdecontenido.appendChild(commentsLink)
     divdecontenido.appendChild(time)
 
 
@@ -174,24 +181,27 @@ async function ponerHistorias() {
 }
 
 async function ponerComentarios(noticia, kids) {
+  isNotReloading = true;
   if (kids === undefined || kids === 0) {
     return
   }
-  let comentariosIds = [];
-  for (let index = 0; index < contador2; index++) {
+
+  let commentIds = [];
+  for (let index = 0; index < commentsPerPage; index++) {
     const element = kids[index];
-    comentariosIds.push(element)
+    commentIds.push(element)
   }
 
   showLoader()
 
   const texto = document.createElement("div")
-  texto.textContent = noticia
+  texto.classList.add("story")
+  texto.textContent = convertidor(noticia)
 
   stories.appendChild(texto)
 
   const comentarios = await Promise.all(
-    comentariosIds.map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(response => response.json())
+    commentIds.map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(response => response.json())
     ))
 
   comentarios.forEach((comentario, i) => {
@@ -200,7 +210,7 @@ async function ponerComentarios(noticia, kids) {
     div.textContent = comentario.by
     const p = document.createElement("p")
     p.innerHTML = convertidor( comentario.text)
-    const a = document.createElement("p")
+    const a = document.createElement("a")
     a.textContent = "Numero Comentarios: " + (comentario.kids?.length || 0)
     a.addEventListener(("click"), () => {
       ponerComentarios(comentario.text, comentario.kids)
@@ -213,22 +223,26 @@ async function ponerComentarios(noticia, kids) {
     stories.appendChild(div)
   })
 
-  let button = document.createElement("button")
-  button.textContent = "More"
+  let buttonContainer = document.createElement("div")
+  buttonContainer.id="more"
+  let moreButton = document.createElement("button")
+  moreButton.textContent = "More"
+  moreButton.classList.add("botones")
 
-  button.addEventListener("click", () => {
-    contador2 = contador2 + 3
+  moreButton.addEventListener("click", () => {
+    commentsPerPage = commentsPerPage + 3
     ponerComentarios(noticia, kids)
   })
 
+    estadodelosbotones.style.display = "none"
 
-
-  stories.appendChild(button)
+  buttonContainer.appendChild(moreButton)
+  stories.appendChild(buttonContainer)
 }
 
 
 function ponerDatosDelAutor(autor) {
-
+  isNotReloading = true;
   showLoader()
 
   const div = document.createElement("div")
@@ -252,6 +266,8 @@ function ponerDatosDelAutor(autor) {
   }
 
   hideLoader()
+      estadodelosbotones.style.display = "none"
+
 
   console.log(autor.about)
 
@@ -265,34 +281,34 @@ function ponerDatosDelAutor(autor) {
 
 function convertidor(frase){
   console.log(frase)
-  const txt = document.createElement("textarea");
-  txt.innerHTML = frase
-  let textolimpio = txt.value
-  console.log(textolimpio)
-  textolimpio = textolimpio.replace(/<p>/gi, "\n") 
-  console.log(textolimpio)
+  const htmlDecoder = document.createElement("textarea");
+  htmlDecoder.innerHTML = frase
+  let cleanText = htmlDecoder.value
+  console.log(cleanText)
+  cleanText = cleanText.replace(/<p>/gi, "\n") 
+  console.log(cleanText)
 
-  return textolimpio
+  return cleanText
 }
 
 
 function loadLocalSelect() {
-  let aaa = localStorage.getItem("selecttype")
+  let savedType = localStorage.getItem("selecttype")
 
-  return aaa
+  return savedType
 }
 
 
 function formatedDate(fecha) {
    let date = new Date(fecha * 1000)
-   let mm = String(date.getUTCMonth()+1).padStart(2,'0');
-   let dd = String(date.getUTCDate()).padStart(2,'0');
-   let yy = String(date.getUTCFullYear());
-   dd = Number(dd)
-   mm = Number(mm)
-   let month = pickMonth(mm)
-   console.log(month  + " " + dd + " " +yy)
-   return `${month} ${dd} ${yy}`
+   let month = String(date.getUTCMonth()+1).padStart(2,'0');
+   let day = String(date.getUTCDate()).padStart(2,'0');
+   let year = String(date.getUTCFullYear());
+   day = Number(day)
+   month = Number(month)
+   let monthName = pickMonth(month)
+   console.log(monthName  + " " + day + " " +year)
+   return `${monthName} ${day} ${year}`
 }
 
 function pickMonth(month ) {
@@ -330,8 +346,10 @@ function formaterTime(time) {
 }
 
 setInterval(async () => {
+  if (!isNotReloading){
   let ids = await pillarHistorias()
-  if (JSON.stringify(ids) !== JSON.stringify(idsnoticias)){
+  if (JSON.stringify(ids) !== JSON.stringify(storyIds)){
     ponerHistorias()
   }
+}
 },300000);
